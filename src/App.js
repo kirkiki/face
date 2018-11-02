ed89a3fdac651a4aea6e846141f104b7b2cb2f37
NokiDev <noki.dev@gmail.com>
2018-11-02 13:32:44 +0100

    Update API call for url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
 
@@ -17,8 +18,56 @@ class App extends Component {
     });
   }
 
-  uploadImage() {
-    console.log(this.state.inputValue)
+  /*post https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect
+header: Ocp-Apim-Subscription-Key --> 59367a363a2947208054fa85fc318ce9
+
+pour une url: Content-type: application/json
+pour une image: application/octet-stream
+
+
+params: returnFaceLandmarks (bool)
+        returnFaceAttributes (age,gender,smile,facialHair,headPose,glasses,emotion,hair,makeup,accessories,blur,exposure,noise) */
+
+  uploadUrlImage() {
+    // TODO add url checks.
+    //
+    //
+    axios.post('/https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect', {
+      url : this.state.inputValue,
+      config : {
+        headers : {
+          "Ocp-Apim-Subscription-Key": "59367a363a2947208054fa85fc318ce9",
+          "Content-type" : "application/json"
+        }
+      }
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  uploadImage(evt) {
+    // TODO parse event;
+    console.log(evt);
+    /*
+    axios.post('/https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect', {
+      url : evt.target.value.name,
+      config : {
+        headers : {
+          "Ocp-Apim-Subscription-Key": "59367a363a2947208054fa85fc318ce9",
+          "Content-type" : "application/json"
+        }
+      }
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });*/
   }
 
   render() {
@@ -34,14 +83,14 @@ class App extends Component {
                 <input id="urlImput" value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)} placeholder="http://your_link/to/an/image.ext" />
               </div>
               <div>
-                <button type="button" onClick={evt => this.uploadImage()}>Submit</button>
+                <button type="button" onClick={evt => this.uploadUrlImage()}>Submit</button>
               </div>
             </div>
             OR
             <div>
               <label>Take a picture of your face</label>
               <div>
-                <input type="file" id="mypic" accept="image/*" capture="camera" />
+                <input type="file" id="mypic" accept="image/*" capture="camera"/>
               </div>
             </div>
           </div>

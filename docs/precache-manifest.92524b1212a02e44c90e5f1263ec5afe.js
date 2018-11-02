f9e3f3ac3062cd3a1f48e70f38681eb164982e42
NokiDev <noki.dev@gmail.com>
2018-11-02 17:54:43 +0100

    Update gh pages

---
+++ docs/precache-manifest.92524b1212a02e44c90e5f1263ec5afe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "920179bd2c986d8ecdc3",
+    "url": "/face/static/js/runtime~main.920179bd.js"
+  },
+  {
+    "revision": "e9db6e694666e44c9a04",
+    "url": "/face/static/js/main.e9db6e69.chunk.js"
+  },
+  {
+    "revision": "6f1a435915ade84db1a2",
+    "url": "/face/static/js/1.6f1a4359.chunk.js"
+  },
+  {
+    "revision": "e9db6e694666e44c9a04",
+    "url": "/face/static/css/main.9eb4c23d.chunk.css"
+  },
+  {
+    "revision": "40bff9900efaf5ecd993e25d55538a2f",
+    "url": "/face/index.html"
+  }
+];
